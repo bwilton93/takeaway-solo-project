36404b70cce146b3cae1b403631f31e8d98debd9
Barney <B.Wilton1993@gmail.com>
2023-04-19 14:47:40 +0100

    send message method defined and time method defined

--- lib/order_confirmation.rb
+++ lib/order_confirmation.rb
@@ -13,4 +13,19 @@ class OrderConfirmation
   def client
     return @twilio_client
   end
+
+  def time
+    @time = Time.now 
+  end
+
+  def send_message(phone_number)
+    time_now = time
+    @twilio_client.messages.create(
+      from: '+16205360794',
+      to: phone_number,
+      body: "Thank you! Your order was placed and will be delivered before #{time_now + (30 * 60)}"
+    )
+  end
 end
+
+p Time.now + (20 * 60)
