05e4de3d82368a89d0bfabf44972ebabe8bab2f3
Barney <B.Wilton1993@gmail.com>
2023-04-19 14:39:04 +0100

    created test for order_confirmation initialization

--- lib/order_confirmation.rb
+++ lib/order_confirmation.rb
@@ -1,5 +1,16 @@
+require 'twilio-ruby'
+
 class OrderConfirmation
   # obtaining current time
   # adding arbitrary value to time
   # sends text message saying order will arrive
+  def initialize
+    @account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    @account_auth = ENV["TWILIO_AUTH_TOKEN"]
+    @twilio_client = Twilio::REST::Client.new(@account_sid, @account_auth)
+  end
+
+  def client
+    return @twilio_client
+  end
 end
--- spec/order_confirmation_spec.rb
+++ spec/order_confirmation_spec.rb
@@ -1,5 +1,8 @@
 require 'order_confirmation'
 
 RSpec.describe OrderConfirmation do
-  
+  it "initialises" do
+    order_confirmation = OrderConfirmation.new
+    expect(order_confirmation.client).to be
+  end
 end
